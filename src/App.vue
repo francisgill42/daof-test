e5676e55bf57784d8abc1830dabf0ee3c0b3a18a
francisgill42 <francisgill42@gmail.com>
2020-09-25 18:27:20 +0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+<v-app>
+<v-app-bar app class="white">
+<v-toolbar-title>
+    <div style="width: 14%; height:55px; margin-left:79px;" >
+    <v-img style="padding:2%;" src="@/assets/header-logo-img.png" />
+    </div>
+
+</v-toolbar-title>
+<v-spacer></v-spacer>
+<v-btn style="margin-right:79px;" class="primary" text href="https://orangeroomdigital.com/salesapp/public/">
+<span>Dashboard (DEV)</span>
+</v-btn>
+</v-app-bar>
+
+<v-main class="mt-5">
+<Form/>
+</v-main>
+</v-app>
+</template>
+
+<script>
+import Form from './components/Form';
+
+export default {
+    name: 'App',
+    components: { Form },
+    data: () => ({
+    title:'Digital Account Opening Form',
+    }),
+};
+</script>
+
+<style>
+.v-image__image{
+    height: 94%;
+}
+</style>
---
+++ src/components/Test.vue
@@ -0,0 +1,57 @@
+<template>
+  <v-simple-table>
+    <template v-slot:default>
+      <thead>
+        <tr>
+          <th class="text-left"><strong>Key</strong></th>
+          <th class="text-left"><strong>Values</strong></th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(item,i) in countries" :key="i">
+          <td>{{ item.CNT_COUNTRYCODE }}</td>
+          <td>{{ item.CNT_OFFICALNAME }}</td>
+        </tr>
+      </tbody>
+    </template>
+  </v-simple-table>
+</template>
+
+<script>
+import axios from 'axios';
+
+  export default {
+    data () {
+      return {
+          countries : []
+       
+      }
+    },
+    created(){
+      let xmls=`<?xml version="1.0" encoding="utf-8"?>
+      <soap12:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap12="http://www.w3.org/2003/05/soap-envelope">
+      <soap12:Body>
+      <GetCountry xmlns="http://tempuri.org/" />
+      </soap12:Body>
+      </soap12:Envelope>`;
+
+      axios.post('https://daofservice.hblasset.com/DigitalAccountOpenTillVerify.asmx?op=GetCountry',
+      xmls,
+      {headers:
+      {'Content-Type': 'text/xml'}
+      }).then(res=>{
+
+      var parser = new DOMParser();
+      var r = parser.parseFromString(res.data,'application/xml');
+
+      this.countries = JSON.parse(r.getElementsByTagName('GetCountryResult')[0].textContent).Table;
+
+      }).catch(err=>{console.log(err)});
+
+   },
+  }
+</script>
+
+<style>
+
+</style>
